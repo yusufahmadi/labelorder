3188e496d4bd6bc21e1b807eb3a0dd61bfa8cdf4
yanto <mrthx.89@gmail.com>
2020-05-12 13:18:08 +0700

    Conflict2 di Merge, Spinner Ganti ke Material

--- app/src/main/java/io/github/yusufahmadi/labelorder/repository/mdlPublic.java
+++ app/src/main/java/io/github/yusufahmadi/labelorder/repository/mdlPublic.java
@@ -1,10 +1,16 @@
 package io.github.yusufahmadi.labelorder.repository;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import io.github.yusufahmadi.labelorder.MainActivity;
+import io.github.yusufahmadi.labelorder.adapter.BahanAdapter;
 import io.github.yusufahmadi.labelorder.model.Bahan;
+import io.github.yusufahmadi.labelorder.model.PO;
 
 public class mdlPublic {
 
@@ -12,4 +18,28 @@ public class mdlPublic {
     public static List<String> ListStrBahan = new ArrayList<String>();
 
     public static MainActivity mainActivity;
+
+    public static long getDateDiff(String oldDate, String newDate) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", new DateFormatSymbols(Locale.US));
+        try {
+            return TimeUnit.DAYS.convert(format.parse(newDate).getTime() - format.parse(oldDate).getTime(), TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    private BahanAdapter SpinBahan;
+    public static String getBahanByID(int id) {
+        String rst="";
+        for(Bahan bhn: ListBahan) {
+            if(bhn.getId()==id) {
+                //found it!
+                rst =bhn.getNama();
+            }
+        }
+        return rst;
+    }
+
+    public static PO[] ListPO = new PO[0];
 }
